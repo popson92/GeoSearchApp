8bc6c9ca74dd597ea569b5c880b580004dd7e4d3
popoolaolusola92@gmail.com <popoolaolusola92@gmail.com>
2019-11-29 14:48:34 +0100

    getting ui prepared

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
-
+//AIzaSyC_AgddD3PBSw1OunhnHCh5A9y438OZpB8
 //degree to ℉ conversion
 let f = document.getElementById("output℉");
 function tempConverter(valNum) {
